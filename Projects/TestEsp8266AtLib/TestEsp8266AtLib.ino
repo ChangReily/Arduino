04ab5035711260a07a72a2a7051f3ffc41f11de2
Chang, Reily <reily.chang@hp.com>
2019-11-06 13:21:18 +0800

    Fix coding error

--- Projects/TestEsp8266AtLib/TestEsp8266AtLib.ino
+++ Projects/TestEsp8266AtLib/TestEsp8266AtLib.ino
@@ -6,22 +6,22 @@
 
 SoftwareSerial EspSerial(10, 11);
 ESP8266AT Esp01(EspSerial);
-AlarmId id;
+#define DEBUG 0
 
 void setup(void)
 {
   String SntpTime;
   Serial.begin(115200);
-//
-//  Serial.print("== Checks Version Information ==\n");
-//  Serial.println(Esp01.ExecATGMR());
-//
-//  Serial.print("\n== UART Configuration ==\n");
-//  Serial.println(Esp01.QueryATUARTCUR());
-//
-//  Serial.print("\n== Current Wi-Fi mode ==\n");
-//  Serial.println(Esp01.QueryATCWMODECUR());
+#if DEBUG
+  Serial.print("== Checks Version Information ==\n");
+  Serial.println(Esp01.ExecATGMR());
 
+  Serial.print("\n== UART Configuration ==\n");
+  Serial.println(Esp01.QueryATUARTCUR());
+
+  Serial.print("\n== Current Wi-Fi mode ==\n");
+  Serial.println(Esp01.QueryATCWMODECUR());
+#endif
   Serial.print("\n== Get Current AP ==\n");
   Serial.println(Esp01.QueryATCWJAPCUR());
 
@@ -34,27 +34,33 @@ void setup(void)
   delay(1000);
   Serial.print("\n== Checks the SNTP Time ==\n");
   Serial.println(Esp01.QueryATCIPSNTPTIME());
-  SntpTime=Esp01.GetSntpTime();
-  
-  setTime(SntpTime.substring(11,13).toInt(),SntpTime.substring(14,16).toInt(),SntpTime.substring(17,19).toInt(),SntpTime.substring(8,10).toInt(),SntpTime.substring(5,7).toInt(), SntpTime.substring(2,4).toInt()); // set time to Saturday 8:29:00am Jan 1 2011
 
-  // create the alarms, to trigger at specific times
+  SntpTime=Esp01.GetSntpTime();
+  setTime(SntpTime.substring(11,13).toInt(),SntpTime.substring(14,16).toInt(),SntpTime.substring(17,19).toInt(),SntpTime.substring(8,10).toInt(),SntpTime.substring(5,7).toInt(), SntpTime.substring(2,4).toInt());
+
+  // Light
   Alarm.alarmRepeat(8,30,0, LightOn);    // 8:30 every day
   Alarm.alarmRepeat(20,30,0, LightOff);  // 20:30 every day
+
+  // Feeder
+  Alarm.alarmRepeat(10,32,0,FeederOn);   
+  Alarm.alarmRepeat(10,32,3,FeederOff); 
+  Alarm.alarmRepeat(20,45,0,FeederOn);   
+  Alarm.alarmRepeat(20,45,3,FeederOff); 
+
+  // Sync Time
+  Alarm.alarmRepeat(7,30,0, SynSntpTime);    // 7:30 every day
   
-  Alarm.alarmRepeat(10,30,0,FeederOn);   // 10:30 every day
-  Alarm.alarmRepeat(17,30,0,FeederOff);  // 17:30 every day
-  
-  // create timers, to trigger relative to when they're created
-  Alarm.timerRepeat(60, Repeats);           // timer for every 15 seconds
-  id = Alarm.timerRepeat(2, Repeats2);      // timer for every 2 seconds
+  // LCD & Temperature
+  Alarm.timerRepeat(60, Repeats);      // timer for every 60 seconds
+
+  // HC-SR501 PIR Sensor
+  Alarm.timerRepeat(2, Repeats2);      // timer for every 2 seconds
 
 }
 
 void loop(void)
 {
-//  Serial.println(Esp01.GetSntpTime());
-//  digitalClockDisplay();
   Alarm.delay(1000); // wait one second between clock display
 }
 
@@ -77,6 +83,12 @@ void printDigits(int digits) {
   if (digits < 10)
     Serial.print('0');
   Serial.print(digits);
+}
+
+void SynSntpTime() {
+  digitalClockDisplay();
+  Serial.println("  [SynTime] - Sync SNTP Time");
+  Serial.println();
 }
 
 // functions to be called when an alarm triggers:
@@ -113,8 +125,5 @@ void Repeats2() {
   Serial.println();
   
 }
-
-
-
 
 
